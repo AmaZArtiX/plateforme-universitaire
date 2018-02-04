8213a6c51c53eb565d853422c72c0c160b375c0e
Simon Bacquet <bacquet.simon@outlook.fr>
2018-02-04 17:31:26 +0100

    Ajout de la navbar

--- index.php
+++ index.php
@@ -3,19 +3,23 @@
   <head>
     <meta charset="utf-8">
     <title>Accueil</title>
-    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/footer.css">
-    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </head>
   <body>
     <?php require("vues/header.vue.php"); ?>
-
-    <div class="content">
-
+    <div class="container">
+      <div class="jumbotron">
+        <h1>Bootstrap Tutorial</h1>
+        <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing
+        responsive, mobile-first projects on the web.</p>
+      </div>
+      <p>This is some text.</p>
+      <p>This is another text.</p>
     </div>
   </body>
   <?php require("vues/footer.vue.php"); ?>
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
 </html>
