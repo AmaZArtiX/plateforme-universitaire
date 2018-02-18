8222843ef3c896e624a658fe39cf4f0c25b0de37
LAMPE Ronan <lampe.ronan@outlook.fr>
2018-02-18 19:40:09 +0100

    Corrections .css header

--- configuration/config.php
+++ configuration/config.php
@@ -3,7 +3,7 @@
   // Connexion à la BDD
   try {
 
-    $bdd = new PDO('mysql:host=localhost;dbname=bd_platuniv', 'root', 'root');
+    $bdd = new PDO('mysql:host=localhost;dbname=bd_platuniv', 'root', '');
 
   } catch(Exception $e) {
 
