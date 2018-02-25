5eda1ef4e8a5b441fbfecbee9fd1f1e9297ec27b
Simon Bacquet <bacquet.simon@outlook.fr>
2018-02-25 11:59:37 +0100

    Modification de l'insertion d'un membre
    Ajout d'un code retour

--- modeles-controleurs/inscription.php
+++ modeles-controleurs/inscription.php
@@ -12,6 +12,7 @@
     $prenom = htmlspecialchars($_POST['prenom']);
     $nom = htmlspecialchars($_POST['nom']);
     $email = htmlspecialchars($_POST['email']);
+    $longueur_mot_de_passe = strlen($_POST['mot_de_passe']);
     $mot_de_passe = sha1($_POST['mot_de_passe']);
     $mot_de_passe_confirmation = sha1($_POST['mot_de_passe_confirmation']);
 
@@ -36,34 +37,38 @@
                 // Vérification de l'existence de l'adresse e-mail
                 if(existe_email($email) ==  0){
 
+                  echo strlen($mot_de_passe);
                   // Vérification de la taille du mot de passe
-                  if(strlen($mot_de_passe) >= 8 && strlen($mot_de_passe) <= 40){
+                  if($longueur_mot_de_passe >= 8 && $longueur_mot_de_passe <= 40){
 
                     // Vérification de l'égalité des mots de passe
                     if($mot_de_passe == $mot_de_passe_confirmation){
 
                       // Insertion du nouveau membre dans la bdd
-                      inserer_membre($prenom, $nom, $email, $mot_de_passe);
+                      if(inserer_membre($prenom, $nom, $email, $mot_de_passe)){
 
-                      /*********************************************************************
-											$header = "MIME-Version: 1.0\r\n";
-											$header .= 'From: "Forum.com" <bacquet.simon@outlook.fr>'."\n";
-											$header .= 'Content-Type:text/html; charset="utf-8"'."\n";
-											$header .= 'Content-Transfer-Encoding: 8bit';
+                        /*********************************************************************
+  											$header = "MIME-Version: 1.0\r\n";
+  											$header .= 'From: "Forum.com" <bacquet.simon@outlook.fr>'."\n";
+  											$header .= 'Content-Type:text/html; charset="utf-8"'."\n";
+  											$header .= 'Content-Transfer-Encoding: 8bit';
 
-											$message='
-											<html>
-												<body>
-										      <p> Félicitations ! Votre compte a bien été créé. </p
-												</body>
-											</html>
-											';
+  											$message='
+  											<html>
+  												<body>
+  										      <p> Félicitations ! Votre compte a bien été créé. </p
+  												</body>
+  											</html>
+  											';
 
-											mail($mail, "Confirmation de création de compte", $message, $header);
-											************************************************************************/
+  											mail($mail, "Confirmation de création de compte", $message, $header);
+  											************************************************************************/
 
-                      $succes = "Vous êtes bien enregistré ! Cliquez <a href=\"../vues/connexion.vue.php\" class=\"alert-link\">ici</a> pour vous connecter.";
+                        $succes = "Vous êtes bien enregistré ! Cliquez <a href=\"../vues/connexion.vue.php\" class=\"alert-link\">ici</a> pour vous connecter.";
+                      } else {
 
+                        $erreur = "Votre inscription a échoué, veuillez ré-essayer ultérieurement.";
+                      }
                     } else {
 
                       $erreur = "Vos mots de passe doivent être identiques";
